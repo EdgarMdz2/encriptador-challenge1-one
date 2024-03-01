bc8ec59d16b91b659e61808d29c49e0c2afa49bf
EdgarMdz2 <edgarmendeez@gmail.com>
2024-03-01 15:16:03 -0600

    Implementando funcion de evaluarAcentos()

--- script.js
+++ script.js
@@ -16,55 +16,79 @@ function cambioPantalla() { // ocultar .caja-preresultado y activar display bloc
     document.getElementById('cajaPreresultado').style = 'display: none';
 }
 
+function reestablecerPantalla() { // mostrar .caja-preresultado y activar display none de <p>
+    document.getElementById('textoResultado').style = 'display: none';
+    document.getElementById('cajaPreresultado').style = 'display: block';
+}
+
+function evaluarAcentos(texto){ //Evalúa la presencia de acentos en el texto
+    const regex = /[A-Z\u00C0-\u00FF]/;
+    if (regex.test(texto)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function encriptar (event) {
     event.preventDefault(); //Evita que la página se recargue
     let arrayTexto = convertirArray(); //Declarando variable con el valor que retorna la función "convertirArray"
-    for (let i = 0; i < arrayTexto.length; i++) { //Recorriendo array y modificando los valores
-        if (arrayTexto[i] == 'a') {
-            arrayTexto[i] = 'ai';
-        } 
-        else if (arrayTexto[i] == 'e') {
-            arrayTexto[i] = 'enter';
-        } 
-        else if (arrayTexto[i] == 'i') {
-            arrayTexto[i] = 'imes';
-        } 
-        else if (arrayTexto[i] == 'o') {
-            arrayTexto[i] = 'ober';
-        } 
-        else if (arrayTexto[i] == 'u') {
-            arrayTexto[i] = 'ufat';
+    if (arrayTexto == '' || evaluarAcentos(textoUsuario.value)) {
+        alert('Ingresa un texto para encriptar. Solo letras minúsculas y sin acentos.');
+        reestablecerPantalla(); // mostrar .caja-preresultado y activar display none de <p>
+    } else {
+        for (let i = 0; i < arrayTexto.length; i++) { //Recorriendo array y modificando los valores
+            if (arrayTexto[i] == 'a') {
+                arrayTexto[i] = 'ai';
+            } 
+            else if (arrayTexto[i] == 'e') {
+                arrayTexto[i] = 'enter';
+            } 
+            else if (arrayTexto[i] == 'i') {
+                arrayTexto[i] = 'imes';
+            } 
+            else if (arrayTexto[i] == 'o') {
+                arrayTexto[i] = 'ober';
+            } 
+            else if (arrayTexto[i] == 'u') {
+                arrayTexto[i] = 'ufat';
+            }
         }
+        const valorDespuesEncriptado = arrayTexto.join(''); //El array resultante se convierte denuevo a string
+        document.getElementById('textoResultado').innerText = valorDespuesEncriptado; //.innerText asigna el valorDepuesEncriptado al elemento "textoResultado"
+        cambioPantalla(); // Ocultar .caja-preresultado y activar display block de <p>
     }
-    const valorDespuesEncriptado = arrayTexto.join(''); //El array resultante se convierte denuevo a string
-    document.getElementById('textoResultado').innerText = valorDespuesEncriptado; //.innerText asigna el valorDepuesEncriptado al elemento "textoResultado"
-    cambioPantalla(); // Ocultar .caja-preresultado y activar display block de <p>
 }
 
 function desencriptar (event) {
     event.preventDefault();
     let arrayTexto = convertirArray(); //Declarando variable con el valor que retorna la función "convertirArray"
-    for (let i = 0; i < arrayTexto.length; i++) { //Recorriendo array y modificando los valores
-        if (arrayTexto[i] == 'a' || arrayTexto[i] == 'e' || arrayTexto[i] == 'i' || arrayTexto[i] == 'o' || arrayTexto[i] == 'u' ) { //Al encontrar una vocal...
-            if (arrayTexto[i] == 'a' && arrayTexto[i+1] == 'i') {
-                arrayTexto.splice(i+1,1);
-                console.log(arrayTexto);
-            }
-            else if (arrayTexto[i] == 'e' && arrayTexto[i+1] == 'n' && arrayTexto[i+2] == 't' && arrayTexto[i+3] == 'e' && arrayTexto[i+4] == 'r') {
-                arrayTexto.splice(i+1,4);
-            }
-            else if (arrayTexto[i] == 'i' && arrayTexto[i+1] == 'm' && arrayTexto[i+2] == 'e' && arrayTexto[i+3] == 's') {
-                arrayTexto.splice(i+1,3);
-            }
-            else if (arrayTexto[i] == 'o' && arrayTexto[i+1] == 'b' && arrayTexto[i+2] == 'e' && arrayTexto[i+3] == 'r') {
-                arrayTexto.splice(i+1,3);
-            }
-            else if (arrayTexto[i] == 'u' && arrayTexto[i+1] == 'f' && arrayTexto[i+2] == 'a' && arrayTexto[i+3] == 't') {
-                arrayTexto.splice(i+1,3);
-            }
-        } 
+    if (arrayTexto == '' || evaluarAcentos(textoUsuario.value)) {
+        alert('Ingresa un texto para desencriptar. Solo letras minúsculas y sin acentos.');
+        reestablecerPantalla(); // mostrar .caja-preresultado y activar display none de <p>
+    } else {
+        for (let i = 0; i < arrayTexto.length; i++) { //Recorriendo array y modificando los valores
+            if (arrayTexto[i] == 'a' || arrayTexto[i] == 'e' || arrayTexto[i] == 'i' || arrayTexto[i] == 'o' || arrayTexto[i] == 'u' ) { //Al encontrar una vocal...
+                if (arrayTexto[i] == 'a' && arrayTexto[i+1] == 'i') {
+                    arrayTexto.splice(i+1,1);
+                    console.log(arrayTexto);
+                }
+                else if (arrayTexto[i] == 'e' && arrayTexto[i+1] == 'n' && arrayTexto[i+2] == 't' && arrayTexto[i+3] == 'e' && arrayTexto[i+4] == 'r') {
+                    arrayTexto.splice(i+1,4);
+                }
+                else if (arrayTexto[i] == 'i' && arrayTexto[i+1] == 'm' && arrayTexto[i+2] == 'e' && arrayTexto[i+3] == 's') {
+                    arrayTexto.splice(i+1,3);
+                }
+                else if (arrayTexto[i] == 'o' && arrayTexto[i+1] == 'b' && arrayTexto[i+2] == 'e' && arrayTexto[i+3] == 'r') {
+                    arrayTexto.splice(i+1,3);
+                }
+                else if (arrayTexto[i] == 'u' && arrayTexto[i+1] == 'f' && arrayTexto[i+2] == 'a' && arrayTexto[i+3] == 't') {
+                    arrayTexto.splice(i+1,3);
+                }
+            } 
+        }
+        const valorDespuesDesencriptado = arrayTexto.join(''); //El array resultante se convierte denuevo a string
+        document.getElementById('textoResultado').innerText = valorDespuesDesencriptado; //.innerText asigna el valorDepuesEncriptado al elemento "textoResultado"
+        cambioPantalla(); // Ocultar .caja-preresultado y activar display block de <p>
     }
-    const valorDespuesDesencriptado = arrayTexto.join(''); //El array resultante se convierte denuevo a string
-    document.getElementById('textoResultado').innerText = valorDespuesDesencriptado; //.innerText asigna el valorDepuesEncriptado al elemento "textoResultado"
-    cambioPantalla(); // Ocultar .caja-preresultado y activar display block de <p>
 }
